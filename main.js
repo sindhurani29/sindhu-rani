932601eb07a833e81672476b8fe3a4e8ddf127d9
sindhu rani goli <saisindhurani@gmail.com>
2020-03-06 16:29:35 -0600

    commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var cel = document.getElementById("txt1").value;
+    var far = cel * 9 / 5 + 32;
+    document.getElementById("calc").innerHTML = far;
+
+}
+
+
+ 
+
+ 
